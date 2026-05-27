3e88e6269aa2d8da84769ecf948bc112c258020d
Juan Valentin <juanvalentinanto12@gmail.com>
2026-05-27 08:51:37 -0600

    fix(juan) : cambios de colores de los botones

--- proyecto_2/tiendita_chida/src/app/components/newProduct/newProduct.ts
+++ proyecto_2/tiendita_chida/src/app/components/newProduct/newProduct.ts
@@ -71,7 +71,7 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
               Cancelar
             </button>
             <button (click)="onSave()" 
-              class="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 transition-colors">
+              class="px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 transition-colors">
               Guardar Producto
             </button>
           </div>
